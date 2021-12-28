7555a7fe89629ff521dd9e836ac91ca0bc847aee
LErnandes <luis78270@gmail.com>
2021-12-27 23:11:04 -0300

    Adding model and admin config

--- dress_app/admin.py
+++ dress_app/admin.py
@@ -1,1 +1,17 @@
 from django.contrib import admin
+from .models import *
+
+class CustomerAdmin(admin.ModelAdmin):
+    search_fields = ['email', 'first_name', 'last_name']
+
+
+class PhoneAdmin(admin.ModelAdmin):
+    search_fields = ['number']
+
+
+class AddressAdmin(admin.ModelAdmin):
+    search_fields = ['address']
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Phone, PhoneAdmin)
+admin.site.register(Address, AddressAdmin)
--- dress_app/models.py
+++ dress_app/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+
+
+class Phone(models.Model):
+    number = models.CharField(max_length=11)
+
+class Address(models.Model):
+    address = models.CharField(max_length=40)
+
+class Customer(models.Model):
+    email = models.EmailField(max_length=30, primary_key=True)
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
+    phone = models.ManyToManyField(Phone)
+    address = models.ManyToManyField(Address)
