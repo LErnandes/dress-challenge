c1eb2e75d74a2073d6684ef802aae588c7796041
LErnandes <luis78270@gmail.com>
2021-12-27 23:09:19 -0300

    Starting the project/app and adding two routes

---
+++ dress_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DressAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dress_app'
---
+++ migrate.sh
@@ -0,0 +1,3 @@
+python3 manage.py makemigrations dress_app
+python3 manage.py sqlmigrate dress_app 0001
+python3 manage.py migrate
